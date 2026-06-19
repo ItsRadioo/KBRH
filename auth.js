48441fd79db111134eb25a047acd8cfcdba94af3
ItsRadioo <chase.lgw.matthews@gmail.com>
2026-06-19 09:57:54 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,5 @@
 const AUTH_KEY = "residentChoreRotator.staffLoggedIn";
-const PASSWORD_HASH = "c62e4615bd39e222c5ed4cb3066e3dbebec559f43b48e16d6b30c95535ce97b4";
-// Default password: changeme
-
-async function sha256(text) {
-  const data = new TextEncoder().encode(text);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-  return Array.from(new Uint8Array(hashBuffer))
-    .map(byte => byte.toString(16).padStart(2, "0"))
-    .join("");
-}
+const STAFF_PASSWORD = "KBRH2026";
 
 function isLoggedIn() {
   return sessionStorage.getItem(AUTH_KEY) === "true";
